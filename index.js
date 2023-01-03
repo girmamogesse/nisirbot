9bd7f59d35ccbb81d8d533b00af4ef67d9b5d5e1
Esubalew Genetu <88676535+su8code@users.noreply.github.com>
2023-01-03 11:55:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -335,3 +335,13 @@ bot.on('contact' , (ctx, next) => {
 })
 
 bot.launch()
+// AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
+exports.handler = async event => {
+  try {
+    await bot.handleUpdate(JSON.parse(event.body))
+    return { statusCode: 200, body: "" }
+  } catch (e) {
+    console.error("error in handler:", e)
+    return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
+  }
+}
